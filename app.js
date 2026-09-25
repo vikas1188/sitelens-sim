468f7374faa91a54d295dde9e77e92a5c98bd8fb
Vikas Singh <vikas.singh1188@gmail.com>
2026-09-25 14:50:53 -0700

    feat: construct procedural site diorama and worker figures

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import {createScene,box,THREE} from './scene.js';
+import {createScene,buildSite,THREE} from './scene.js';
 const view=createScene(document.querySelector('#viewport'));
-box(view.scene,50,.5,50,0,-.3,0,0xb5bba7);view.scene.add(new THREE.GridHelper(50,25,0x939d8f,0xa6af9c));box(view.scene,3,3,3,0,1.5,0,0xe8ad36);
+const site=buildSite(view.scene);
 document.querySelector('#loading').remove();function loop(){requestAnimationFrame(loop);view.render()}loop();
